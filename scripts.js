ae0aa1b70f57c71bea3e2df21735eeda2ea894ef
Jessie Lesbian <jessietessie256@gmail.com>
2021-08-20 11:36:32 +0700

    Updated User Interface

--- scripts.js
+++ scripts.js
@@ -13,21 +13,38 @@ var loadedAccount = null;
 const BigInt = web3.utils.BN;
 const rawUnitsToken = new BigInt("1000000000000");
 const nativeBalance = document.getElementById("nativeBalance");
+const sendEubiButton = document.getElementById("sendEubiButton");
+const sendEubiPreloader = document.getElementById("sendEubiPreloader");
 var walletAddressRAW = "0x0000000000000000000000000000000000000000";
 var privateKeyRAW = "";
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
+const collapsibleOptions = [];
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.collapsible');
+	var instances = M.Collapsible.init(elems, collapsibleOptions);
+});
 const loadTokenContractIMPL = function(address){
 	return new web3.eth.Contract(JSON.parse('[{"constant": false,"inputs": [{"name": "spender","type": "address"},{"name": "value","type": "uint256"}],"name": "approve","outputs": [{"name": "","type": "bool"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [],"name": "totalSupply","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": false,"inputs": [{"name": "from","type": "address"},{"name": "to","type": "address"},{"name": "value","type": "uint256"}],"name": "transferFrom","outputs": [{"name": "","type": "bool"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [{"name": "who","type": "address"}],"name": "balanceOf","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": false,"inputs": [{"name": "to","type": "address"},{"name": "value","type": "uint256"}],"name": "transfer","outputs": [{"name": "","type": "bool"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [{"name": "owner","type": "address"},{"name": "spender","type": "address"}],"name": "allowance","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"},{"anonymous": false,"inputs": [{"indexed": true,"name": "from","type": "address"},{"indexed": true,"name": "to","type": "address"},{"indexed": false,"name": "value","type": "uint256"}],"name": "Transfer","type": "event"},{"anonymous": false,"inputs": [{"indexed": true,"name": "owner","type": "address"},{"indexed": true,"name": "spender","type": "address"},{"indexed": false,"name": "value","type": "uint256"}],"name": "Approval","type": "event"}]'), address);
 };
 const createWallet = async function(){
 	walletMessage.innerHTML = "Your private key is: " + web3.eth.accounts.create().privateKey + "<br/>Please store the private key somewhere safe, since you need it to unlock your wallet.";
 };
-const refreshTokenBalance = async function(tokenAddress, tokenBalanceElement, walletAddress, tokenName){
-	loadingTokenContract = loadedTokenContracts[tokenAddress];
+const loadTokenContract = function(address){
+	loadingTokenContract = loadedTokenContracts[address];
 	if(loadingTokenContract == undefined){
-		loadingTokenContract = loadTokenContractIMPL(tokenAddress);
-		loadedTokenContracts[tokenAddress] = loadingTokenContract;
+		loadingTokenContract = loadTokenContractIMPL(address);
+		loadedTokenContracts[address] = loadingTokenContract;
 	}
-	loadingTokenContract.methods.balanceOf(walletAddress).call().then(function(value){
+	return loadingTokenContract;
+}
+const refreshTokenBalance = async function(tokenAddress, tokenBalanceElement, walletAddress, tokenName){
+	loadTokenContract(tokenAddress).methods.balanceOf(walletAddress).call().then(function(value){
 		vl = value.length;
 		if(vl > 12){
 			vl -= 12;
@@ -42,7 +59,7 @@ const refreshTokenBalance = async function(tokenAddress, tokenBalanceElement, wa
 };
 const reloadWallet = async function(){
 	myWalletAddress.innerHTML = "Your wallet address is: " + walletAddressRAW;
-	afterWalletLoad.style.visibility = "visible";
+	afterWalletLoad.style.display = "block";
 	refreshTokenBalance("0x8AFA1b7a8534D519CB04F4075D3189DF8a6738C1", eubiBalance, walletAddressRAW, "EUBI");
 	web3.eth.getBalance(walletAddressRAW).then(function(value){
 		vl = value.length;
@@ -73,14 +90,15 @@ const loadWallet = async function(){
 	}
 };
 const showInnerMenu = async function(menu){
-	var temp = async function(menu1, menu2){
+	var temp = function(menu1, menu2, preferredDisplay){
 		if(menu1 == menu2){
-			document.getElementById(menu1).style.visibility = "visible";
+			document.getElementById(menu1).style.display = preferredDisplay;
 		} else{
-			document.getElementById(menu1).style.visibility = "hidden";
+			document.getElementById(menu1).style.display = "none";
 		}
 	};
-	temp("sendeubi", menu);
+	temp("sendeubi", menu, "block");
+	temp("setAllowance", menu, "block");
 };
 const convDecimalToRaw = function(value){
 	value = value.split(".");
@@ -90,20 +108,76 @@ const convDecimalToRaw = function(value){
 	return new BigInt(value[0]).mul(rawUnitsToken).add(new BigInt(value[1].padEnd(12, "0"))).toString();
 };
 const sendeubitx = async function(){
+	sendEubiButton.disabled = true;
+	sendEubiPreloader.style.visibility = "visible";
 	//write transaction
+	sendEubiMessage.innerHTML = "Writing transaction...";
 	var transaction = {};
 	transaction.data = loadedTokenContracts["0x8AFA1b7a8534D519CB04F4075D3189DF8a6738C1"].methods.transfer(sendto.value, convDecimalToRaw(eubiamount.value)).encodeABI();
 	transaction.gas = "100000"
 	transaction.to = "0x8AFA1b7a8534D519CB04F4075D3189DF8a6738C1";
 	transaction.privateKey = privateKeyRAW;
+	sendEubiMessage.innerHTML = "Signing transaction...";
 	//sign and send transaction
 	web3.eth.accounts.signTransaction(transaction, privateKeyRAW).then(function(value){
+		sendEubiMessage.innerHTML = "Sending transaction...";
 		web3.eth.sendSignedTransaction(value.rawTransaction).then(function(value){
-			window.alert("Transaction sent successfully!");
+			if(value === null){
+				sendEubiMessage.innerHTML = "Transaction sent successfully!";
+				sendEubiButton.disabled = false;
+				sendEubiPreloader.style.visibility = "hidden";
+			} else{
+				sendEubiMessage.innerHTML = "Transaction sent successfully! <a href=\"https://www.mintme.com/explorer/tx/" + value.transactionHash + "\">view on blockchain explorer</a>";
+				sendEubiButton.disabled = false;
+				sendEubiPreloader.style.visibility = "hidden";
+			}
+			reloadWallet();
 		}, function(error){
-			window.alert("Can't send transaction!");
+			sendEubiMessage.innerHTML = "Can't send transaction!";
+			sendEubiButton.disabled = false;
+			sendEubiPreloader.style.visibility = "hidden";
+			reloadWallet();
 		});
 	}, function(error){
-		window.alert("Can't sign transaction!");
+		sendEubiMessage.innerHTML = "Can't sign transaction!";
+		sendEubiButton.disabled = false;
+		sendEubiPreloader.style.visibility = "hidden";
+	});
+};
+const approve4eubi = async function(){
+	sendEubiButton.disabled = true;
+	sendEubiPreloader.style.visibility = "visible";
+	//write transaction
+	sendEubiMessage.innerHTML = "Writing transaction...";
+	var transaction = {};
+	transaction.data = loadedTokenContracts["0x8AFA1b7a8534D519CB04F4075D3189DF8a6738C1"].methods.approve(sendto.value, convDecimalToRaw(eubiamount.value)).encodeABI();
+	transaction.gas = "100000"
+	transaction.to = "0x8AFA1b7a8534D519CB04F4075D3189DF8a6738C1";
+	transaction.privateKey = privateKeyRAW;
+	sendEubiMessage.innerHTML = "Signing transaction...";
+	//sign and send transaction
+	web3.eth.accounts.signTransaction(transaction, privateKeyRAW).then(function(value){
+		sendEubiMessage.innerHTML = "Sending transaction...";
+		web3.eth.sendSignedTransaction(value.rawTransaction).then(function(value){
+			if(value === null){
+				sendEubiMessage.innerHTML = "Transaction sent successfully!";
+				sendEubiButton.disabled = false;
+				sendEubiPreloader.style.visibility = "hidden";
+			} else{
+				sendEubiMessage.innerHTML = "Transaction sent successfully! <a href=\"https://www.mintme.com/explorer/tx/" + value.transactionHash + "\">view on blockchain explorer</a>";
+				sendEubiButton.disabled = false;
+				sendEubiPreloader.style.visibility = "hidden";
+			}
+			reloadWallet();
+		}, function(error){
+			sendEubiMessage.innerHTML = "Can't send transaction!";
+			sendEubiButton.disabled = false;
+			sendEubiPreloader.style.visibility = "hidden";
+			reloadWallet();
+		});
+	}, function(error){
+		sendEubiMessage.innerHTML = "Can't sign transaction!";
+		sendEubiButton.disabled = false;
+		sendEubiPreloader.style.visibility = "hidden";
 	});
 };
